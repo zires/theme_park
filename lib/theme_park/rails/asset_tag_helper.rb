f50f79b8d8335b16d58c1a31513b17c390c2ba67
Thierry Zires <zshuaibin@gmail.com>
2012-04-06 17:53:12 +0800

    Temp add some asset method

--- lib/theme_park/rails/asset_tag_helper.rb
+++ lib/theme_park/rails/asset_tag_helper.rb
@@ -4,6 +4,36 @@ module ThemePark
     module AssetTagHelper
       extend ActiveSupport::Concern
 
+      #include Sprockets::Helpers::RailsHelper
+
+      #
+      def theme_stylesheet_link_tag(*sources)
+
+      end
+
+      #
+      def theme_javascript_include_tag(*sources)
+        
+      end
+
+      #
+      def theme_image_path(source, theme = nil)
+        return image_path(source) if theme.blank?
+        if serve_static_assets?
+          
+        end
+      end
+
+      # sprocket assets pipeline enabled or not.
+      def assets_enabled?
+        Rails.application.config.assets.enabled
+      end
+
+      # serve static assets or not.
+      def serve_static_assets?
+        Rails.application.config.serve_static_assets
+      end
+
     end
   end
 end
